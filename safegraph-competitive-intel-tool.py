24fd728bf5a076a7ffe57ede18244eed805933c0
Jeff C. Ho <jeff@safegraph.com>
2022-06-30 10:20:50 -0400

    Update main page with a link to the new page

--- safegraph-competitive-intel-tool.py
+++ safegraph-competitive-intel-tool.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.markdown("# Welcome to the SafeGraph Competitive Intel Apps page :tada:")
-st.sidebar.markdown("# Select an app above 🎈")
+st.markdown("# You've reached this page via an old link!")
+st.markdown("Go [here](https://share.streamlit.io/jennydoan/safegraph-competitive-intel-tool/main/safegraph-competitive-intel-tool.py) for the most up-to-date Apps page!")
